10cffc8c2d294cde939d28a1355eb6008544dd2c
saurabhrathor <saurabh.rathor092@gmail.com>
2020-08-26 09:56:42 +0000

    current day filter added

--- stock_extract.py
+++ stock_extract.py
@@ -3,9 +3,11 @@ import pandas as pd
 #from google.cloud import bigquery
 from pandas.io import gbq
 import pdb
+import datetime
 pdb.set_trace()
 
 ALPHA_VANTAGE_API_KEY = '47MP5C3EGNFQ177U'
+yesterday = (datetime.datetime.today() - datetime.timedelta(days=1)).strftime('%Y-%m-%d')
 
 ts = TimeSeries(key=ALPHA_VANTAGE_API_KEY, output_format='pandas')
 
@@ -16,7 +18,9 @@ intraday_data_sort = intraday_data.sort_index()
 intraday_data_sort.columns = ['volume', 'open', 'high', 'close', 'low']
 intraday_data_sort.reset_index(level=0, inplace=True)
 
-intraday_data_sort.to_csv('dump.csv')
+intraday_data_filter = intraday_data_sort[intraday_data_sort['date'] > yesterday]
+
+intraday_data_filter.to_csv('dump.csv')
 
 # Construct a BigQuery client object.
-intraday_data_sort.to_gbq(destination_table = 'stock_data.google_stock_raw', project_id = 'fifth-listener-285620', if_exists='append')
+intraday_data_filter.to_gbq(destination_table = 'stock_data.google_stock_raw', project_id = 'fifth-listener-285620', if_exists='append')
